7f6499c2032ee61ca410caaaf714574a0b8b8591
Ryan May <112563297+ryma2fhir@users.noreply.github.com>
2026-04-30 10:06:03 +0100

    Clean up debug prints in download_package function
    Removed debug print statements for response status and content.

--- FHIRValidationAction/scripts/configure-packages.py
+++ FHIRValidationAction/scripts/configure-packages.py
@@ -39,8 +39,6 @@ def check_package_locally(package_id, version):
 def download_package(package_id, version, failed):
     url = f"https://packages.simplifier.net/{package_id}/{version}"
     response = requests.get(url)
-    print(response.status_code)
-    print(response)
     
     if response.status_code == 404:
         print(f"Package {package_id}#{version} not found on registry")
@@ -50,6 +48,7 @@ def download_package(package_id, version, failed):
     with open(f"{test_script_repo_path}/packages/{package_id}-{version}.tgz", "wb") as f:
         f.write(response.content)
         return True
+        
     
 def install_package(package_id, version, server_url, failed):
     package_path = f"{test_script_repo_path}/packages/{package_id}-{version}.tgz"
